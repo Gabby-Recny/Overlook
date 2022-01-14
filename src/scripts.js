aba24b9f23fb9f055ed5c5ad25fdfdefa065abcf
Gabby Recny <gjtrecny@gmail.com>
2022-01-13 21:15:54 -0600

    Put code into webpack folders and import bundle.js into html

--- src/scripts.js
+++ src/scripts.js
@@ -3,10 +3,10 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import './css/index.scss';
+
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
