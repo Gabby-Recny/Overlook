d95351de2a162f43a9924e1a6b39f41f26c3907e
Gabby Recny <gjtrecny@gmail.com>
2022-01-15 12:08:48 -0600

    Display User name to dashboard

--- src/scripts.js
+++ src/scripts.js
@@ -21,29 +21,72 @@ import {
   navLogOut,
   mainBookingBtn,
 } from './domUpdates.js';
+import User from './classes/User.js';
 
-
+let randomUser;
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
-
 const fetchData = () => {
-  console.log("PAGE LOAD")
   Promise.all([
     fetchCustomersAPI(),
     fetchOneCustomerAPI(2),
     fetchBookingAPI(),
     fetchRoomsAPI(),
-  ]).then(data => pageLoad(data))
+  ]).then(data => {
+    console.log("Customers", data[0])
+    console.log("Single Customer", data[1])
+    console.log("Bookings", data[2])
+    console.log("Rooms", data[3])
+    randomUser = new User(data[1]);
+    getRandomUser();
+  }).catch(error => console.log(error));
+  console.log("Random USer", randomUser)
+
+}
+// const pageLoad = (data) => {
+//   console.log(data)
+//   // msgUserName.innerHTML = "MICHA";
+//   randomUser = new User(data[0][getRandomIndex(data[0])]);
+//   getRandomUser();
+//
+// }
+
+
+
+
+
+const msgUserName = document.getElementById('msgUserName');
+
+const getRandomIndex = (arr) => {
+  return Math.floor(Math.random() * arr.length);
 }
 
-const pageLoad = (data) => {
-  console.log(data)
-
+const getRandomUser = () => {
+    msgUserName.innerHTML = `${randomUser.name}!`;
 }
+
+console.log('This is the JavaScript entry file - your code begins here.');
+
+// const fetchData = () => {
+//   console.log("PAGE LOAD")
+//   Promise.all([
+//     fetchCustomersAPI(),
+//     fetchOneCustomerAPI(2),
+//     fetchBookingAPI(),
+//     fetchRoomsAPI(),
+//   ]).then(data => pageLoad(data))
+// }
+//
+// const pageLoad = (data) => {
+//   console.log(data)
+//   // msgUserName.innerHTML = "MICHA";
+//   randomUser = new User(data[0][getRandomIndex(data[0])]);
+//   getRandomUser();
+//
+// }
 
 
 window.addEventListener('load', fetchData);
